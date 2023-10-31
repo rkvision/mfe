4cf8cd2cadbd1ff51f8aaf7e400990cc761903b9
Gaivio <ravgu10@gmail.com>
2023-10-31 20:59:58 +0530

    Fix container auth remoteEntry path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
             name: 'auth',
             remotes: {
                 marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: 'auth@${domain}/auth/latest/remoteEntry.js'
+                auth: `auth@${domain}/auth/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
